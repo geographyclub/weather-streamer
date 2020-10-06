a2f25c038ca244a473e6e58280acbaf0770c844e
steve <emailstevekim@gmail.com>
2020-10-06 13:27:19 -0400

    updated

--- streamer.sh
+++ streamer.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 #get_metar.sh
 #get_gdps.sh
-height=512
-width=1024
 
-### misc variables
-#mytime=$(echo "$(date +%H) - ($(date +%H)%3)" | bc | awk '{ printf("%03d", $1) }')
+### misc
+#gdpstime_round=$(echo "$(date +%H) - ($(date +%H)%3)" | bc | awk '{ printf("%03d", $1) }')
 #gdpstime=$(echo ${file} | sed -e 's/^.*_P//g' -e 's/\.grib2//g')
-#gdalwarp -ts ${height} ${width} -r cubicspline
-
-### make color gradients
 #convert -size 10x1024 gradient:navy-snow $PWD/../data/colors/ice-sea.png
 
-### slideshow from images
-convert -delay 50 $PWD/../data/qgis/places/*.png $PWD/../places.gif
+### slideshow qgis
+convert -delay 50 $PWD/../data/qgis/places/*.png $PWD/../places_$(date +%m_%d_%H%M).gif
 
-### gdps animation
-# unprojected
-field=TCDC
-color=$PWD/../data/colors/white-black.txt
+### animate gdps
+height=512
+width=1024
+samplemethod=cubicspline
+colorfile=$PWD/../data/colors/white-black.txt
+input0=$PWD/../data/maps/HYP_HR_SR_OB_DR_1500_751.tif
 counter=1
+field=TCDC
+proj=$(echo '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+#proj=$(echo '+proj=ortho +lat_0=-10 +lon_0=-60')
+# make input(s)
+if [ ! -f ${input0%.*}_${width}_${height}_${samplemethod}.tif ]; then
+  gdalwarp -overwrite -ts ${width} ${height} -r ${samplemethod} -t_srs "${proj}" ${input0} ${input0%.*}_${width}_${height}_${samplemethod}.tif
+  exit 0
+fi
+# make frames 
 rm -f $PWD/../data/tmp/*.tif
 ls $PWD/../data/gdps/*${field}*.grib2 | while read file; do
-  gdaldem color-relief -f 'GRIB' -of 'GTiff' --config GDAL_PAM_ENABLED NO ${file} ${color} /vsistdout/ | gdalwarp -overwrite -f 'GTiff' -of 'GTiff' --config GDAL_PAM_ENABLED NO -co PROFILE=BASELINE /vsistdin/ $PWD/../data/tmp/$(printf "%06d" ${counter}).tif
+  gdaldem color-relief -alpha -f 'GRIB' -of 'GTiff' --config GDAL_PAM_ENABLED NO ${file} ${colorfile} /vsistdout/ | gdalwarp -overwrite -f 'GTiff' -of 'GTiff' -ts ${width} ${height} -r ${samplemethod} --config GDAL_PAM_ENABLED NO -co PROFILE=BASELINE /vsistdin/ $PWD/../data/tmp/$(printf "%06d" ${counter}).tif
   (( counter = counter + 1 ))
 done
-ffmpeg -y -r 10 -i $PWD/../data/tmp/%06d.tif -s ${width}x${height} -c:v libx264 -crf 23 -pix_fmt yuv420p -preset fast -threads 0 -movflags +faststart $PWD/../${field}_unprojected.mp4
+# make video
+ffmpeg -y -r 10 -i ${input0%.*}_${width}_${height}_${samplemethod}.tif -i $PWD/../data/tmp/%06d.tif -filter_complex "[0:v][1:v] overlay=W-w:H-h" -s ${width}x${height} -c:v libx264 -crf 23 -pix_fmt yuv420p -preset fast -threads 0 -movflags +faststart $PWD/../${field}_$(date +%m_%d_%H%M).mp4
 
-# orthographic
-field=TCDC
-color=$PWD/../data/colors/white-black.txt
-counter=1
-rm -f $PWD/../data/tmp/*.tif
-ls $PWD/../data/gdps/*${field}*.grib2 | while read file; do
-  gdaldem color-relief -f 'GRIB' -of 'GTiff' --config GDAL_PAM_ENABLED NO ${file} ${color} /vsistdout/ | gdalwarp -overwrite -f 'GTiff' -of 'GTiff' --config GDAL_PAM_ENABLED NO -co PROFILE=BASELINE -t_srs '+proj=ortho +lat_0=-10 +lon_0=-60' /vsistdin/ $PWD/../data/tmp/$(printf "%06d" ${counter}).tif
-  (( counter = counter + 1 ))
-done
-ffmpeg -y -r 10 -i $PWD/../data/tmp/%06d.tif -vf "scale=${width}:-2" -c:v libx264 -crf 23 -pix_fmt yuv420p -preset fast -threads 0 -movflags +faststart $PWD/../${field}_ortho_west.mp4
+############## TODO ##############
+
+# title
+
+# captions
+
+# ticker
+
+# subset data 
 
 
 ### imagemagick
